2bdc6e829bf616ad2fad4824c058d4b7e8306e35
yuniorlaureano <yuniorlaureano@gmail.com>
2024-09-06 03:39:03 -0400

    fix user profile

--- GPA.Data/Security/GPAUserRepository.cs
+++ GPA.Data/Security/GPAUserRepository.cs
@@ -57,7 +57,8 @@ namespace GPA.Data.Security
 	                UserName,
 	                Photo,
 	                Email,
-	                Deleted
+	                Deleted,
+                    CAST(0 AS BIT ) IsAssigned
                 FROM [GPA].[Security].[Users]
                 WHERE 
                     Id = @Id    
