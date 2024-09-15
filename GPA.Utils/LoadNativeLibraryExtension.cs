5b5dbe3c3398ee94f48d505af9aa0d8672b71a53
yuniorlaureano <yuniorlaureano@gmail.com>
2024-09-15 17:26:10 -0400

    copy dinktopdf dll to root

--- GPA.Utils/LoadNativeLibraryExtension.cs
+++ GPA.Utils/LoadNativeLibraryExtension.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System.Runtime.InteropServices;
 
 namespace GPA.Utils
@@ -8,39 +7,18 @@ namespace GPA.Utils
     {
         public static void LoadDinkToPdfNativeLibrary(this IServiceCollection services, string rootPath)
         {
-            var architecture = RuntimeInformation.ProcessArchitecture;
-            var basePath = AppContext.BaseDirectory;
-            var libraryPath = string.Empty;
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            var dll = "libwkhtmltox.dll";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                libraryPath = architecture == Architecture.X64
-                    ? Path.Combine(basePath, "libs", "dinktopdflibs", "64 bit", "libwkhtmltox.dll")
-                    : Path.Combine(basePath, "libs", "dinktopdflibs", "32 bit", "libwkhtmltox.dll");
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                libraryPath = architecture == Architecture.X64
-                    ? Path.Combine(basePath, "libs", "dinktopdflibs", "64 bit", "libwkhtmltox.so")
-                    : Path.Combine(basePath, "libs", "dinktopdflibs", "32 bit", "libwkhtmltox.so");
+                dll = "libwkhtmltox.so";
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                libraryPath = architecture == Architecture.X64
-                    ? Path.Combine(basePath, "libs", "dinktopdflibs", "64 bit", "libwkhtmltox.dylib")
-                    : Path.Combine(basePath, "libs", "dinktopdflibs", "32 bit", "libwkhtmltox.dylib");
+                dll = "libwkhtmltox.dylib";
             }
 
             var context = new CustomAssemblyLoadContext();
-            context.LoadUnmanagedLibrary(libraryPath);
-
-            // Inject logger to log
-            var loggerFactory = services.BuildServiceProvider().GetService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger("DinkToPdf");
-            logger.LogInformation("***********************************************************************************************************");
-            logger.LogInformation("Native library loaded successfully.");
-            logger.LogInformation(rootPath);
-            logger.LogInformation(basePath);
+            context.LoadUnmanagedLibrary(Path.Combine(AppContext.BaseDirectory, dll));
         }
     }
 }
